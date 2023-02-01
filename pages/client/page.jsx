8f08b9a06093bf07abba76733794ca85483a584d
Gerelt-Od <gereltod.s@gmail.com>
2023-02-01 12:01:07 +0800

    asdf

---
+++ pages/client/page.jsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { useState } from 'react'
+import useSWR from "swr"
+import Link from "next/link"
+import Form from '../../components/form'
+
+const fetcher = (path) => fetch(`https://api.gtnjob.tk/api/${path}`).then(res => res.json())
+
+export default function Clientpage() {
+	const characters = useSWR("reference", fetcher)
+
+  const [errorMsg, setErrorMsg] = useState('')
+
+	async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (errorMsg) setErrorMsg('')
+
+    const body = {
+      email: "gereltod.s@gmail.com",
+      password_hash: "Asdf@123",
+    }
+
+    try {
+			console.log(JSON.stringify(body))
+      const res = await fetch('https://api.gtnjob.tk/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+				// host: 'localhost',
+				// mode: 'no-cors',
+        body: JSON.stringify(body),
+      })
+      if (res.status === 200) {
+				console.log(res);
+        //Router.push('/')
+      } else {
+        throw new Error(await res.text())
+      }
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+      setErrorMsg(error.message)
+    }
+  }
+
+	return (
+		<div>
+			<div className="login">
+        <Form isLogin errorMessage={errorMsg} onSubmit={handleSubmit} />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+			<h2>Client Fetching</h2>
+			{characters?.data?.map(c =>
+				<ul key={c.list_id}>
+					<Link href={`/staticprops/${c.list_name}`}>
+						<li>
+							{c.list_name}
+						</li>
+					</Link>
+				</ul>
+			)}
+		</div>
+	)
+}
+
+
---
+++ pages/server/page.jsx
@@ -0,0 +1,24 @@
+//getServerSideProps in Next13
+import { use } from "react"
+
+async function getCharacters() {
+	return await (await fetch("https://api.gtnjob.tk/api/reference", { cache: "no-store" })).json();
+}
+
+export default function Serverpage() {
+	const characters = use(getCharacters());
+	console.log(characters)
+	return (
+		<div>
+			<h2>Server Fetching (getServerSideProps)</h2>
+			{characters?.map((c) => {
+				return (
+					<ul key={c.list_id}>
+						<li>{c.list_name}</li>
+					</ul>
+				)
+			})}
+		</div>
+	)
+}
+
